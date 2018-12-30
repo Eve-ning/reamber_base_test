9df0406c4f83afde4ba8e225eeadf7a5b09d25f5
Eve-ning <johnchangqi@hotmail.com>
2018-12-30 20:01:15 +0800

    Added Vector Test Methods

--- unit_tests.cpp
+++ unit_tests.cpp
@@ -13,6 +13,7 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 class mock_objects {
 public:
 	mock_objects() {
+
 		hit_object_note.load_parameters(
 			0, 192, 1000, 1, osu_object::sample_set::AUTO, 0,
 			osu_object::sample_set::AUTO, osu_object::sample_set::AUTO,
@@ -21,17 +22,41 @@ public:
 			0, 192, 1000, 1, osu_object::sample_set::AUTO, 1500,
 			osu_object::sample_set::AUTO, osu_object::sample_set::AUTO,
 			osu_object::sample_set::AUTO, 50, "hitsound.wav", 4);
-		editor_hit_object_singular.load_parameters(1, 1000, 0, 4);
-		editor_hit_object_multiple.push_back(hit_object());
-		editor_hit_object_multiple.push_back(hit_object());
-		editor_hit_object_multiple[0].load_parameters(1, 1000, 0, 4);
-		editor_hit_object_multiple[1].load_parameters(2, 2000, 0, 4);
+
 		timing_point_sv.load_parameters(
 			1000, 2, 4, osu_object::sample_set::AUTO, 0, 50, false, false
 		);
 		timing_point_bpm.load_parameters(
 			1000, 300, 4, osu_object::sample_set::AUTO, 0, 50, true, false
 		);
+
+		editor_hit_object_singular.load_parameters(1, 1000, 0, 4);
+		editor_hit_object_multiple[0].load_parameters(1, 1000, 0, 4);
+		editor_hit_object_multiple[1].load_parameters(2, 2000, 0, 4);
+
+		hit_object_multiple[0].load_parameters(
+			0, 192, 1000, 1, osu_object::sample_set::AUTO, 0,
+			osu_object::sample_set::AUTO, osu_object::sample_set::AUTO,
+			osu_object::sample_set::AUTO, 40, "hit1.wav", 4);
+		hit_object_multiple[1].load_parameters(
+			2, 192, 2000, 1, osu_object::sample_set::AUTO, 2500,
+			osu_object::sample_set::AUTO, osu_object::sample_set::AUTO,
+			osu_object::sample_set::AUTO, 50, "hit2.wav", 4);
+		hit_object_multiple[2].load_parameters(
+			3, 192, 3000, 1, osu_object::sample_set::AUTO, 0,
+			osu_object::sample_set::AUTO, osu_object::sample_set::AUTO,
+			osu_object::sample_set::AUTO, 60, "hit3.wav", 4);
+
+		timing_point_multiple[0].load_parameters(
+			0, 400, 4, osu_object::sample_set::NORMAL, 1, 50, true, false
+		);
+		timing_point_multiple[1].load_parameters(
+			1000, 2, 4, osu_object::sample_set::NORMAL, 1, 50, false, true
+		);
+		timing_point_multiple[2].load_parameters(
+			2000, 0.5, 4, osu_object::sample_set::NORMAL, 1, 50, false, false
+		);
+
 	}
 	std::string raw_hit_object_str_note =
 		"64,192,1000,1,0,0:0:0:50:hitsound.wav";
@@ -46,12 +71,25 @@ public:
 	std::string editor_hit_object_str_multiple =
 		"00:01:000 (1000|1,2000|2) -";
 
+	std::string raw_hit_object_str_multiple =
+		"64,192,1000,1,0,0:0:0:40:hit1.wav\n" // N
+		"320,192,2000,128,0,2500:0:0:0:70:hit2.wav\n" // LN
+		"448,192,3000,1,0,0:0:0:60:hit3.wav"; // N
+
+	std::string raw_timing_point_str_multiple =
+		"0,150,4,1,1,50,1,0\n" // BPM 400
+		"1000,-50,4,1,1,50,0,1\n" // SV 2.0 Kiai
+		"2000,-200,4,1,1,50,0,0"; // SV 0.50
+
 	hit_object hit_object_note;
 	hit_object hit_object_long_note;
 	timing_point timing_point_sv;
 	timing_point timing_point_bpm;
 	hit_object editor_hit_object_singular;
-	hit_object_v editor_hit_object_multiple;
+	hit_object_v editor_hit_object_multiple = hit_object_v(2);
+
+	hit_object_v hit_object_multiple = hit_object_v(3);
+	timing_point_v timing_point_multiple = timing_point_v(3);
 
 };
 
@@ -100,8 +138,18 @@ namespace reamber_base_test
 
 		}
 
-		//TEST_METHOD(hit_object_v_raw_loading_test);
-		//TEST_METHOD(timing_point_v_raw_loading_test);
+		TEST_METHOD(hit_object_v_raw_loading_test) {
+			hit_object_v ho_v;
+			ho_v.load_raw_hit_object(mocks.raw_hit_object_str_multiple, 4);
+
+			Assert::IsTrue(ho_v == mocks.hit_object_multiple);
+		}
+		TEST_METHOD(timing_point_v_raw_loading_test) {
+			timing_point_v tp_v;
+			tp_v.load_raw_timing_point(mocks.raw_timing_point_str_multiple);
+
+			Assert::IsTrue(tp_v == mocks.timing_point_multiple);
+		}
 		//TEST_METHOD(hit_object_v_editor_loading_test);
 
 	};
